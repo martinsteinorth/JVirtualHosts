1c74287ecefdf57b890cbb6a160d416c6cf1b578
Mario Mueller <mario.mueller.mac@me.com>
2011-04-05 10:59:55 +0200

    Started GUI abstraction layer

---
+++ src/main/java/net/launchpad/jvirtualhosts/gui/LinuxHostFileDataProvider.java
@@ -0,0 +1,11 @@
+package net.launchpad.jvirtualhosts.gui;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mario
+ * Date: 04.04.11
+ * Time: 11:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class LinuxHostFileDataProvider {
+}
